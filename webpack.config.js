a61fd81148a1d7eff903bbbef332c0ebdbdd8517
skb003v <srikanth.barenkala@walmartlabs.com>
2019-08-01 23:54:30 +0530

    split vendor and normal code - wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
 var webpack = require('webpack');
 var path = require('path');
 
+const VENDOR_LIBS = [
+  "faker",
+  "lodash",
+  "react",
+  "react-dom",
+  "react-input-range",
+  "react-redux",
+  "react-router",
+  "redux",
+  "redux-form",
+  "redux-thunk"
+];
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'// here the output files will be created from key names of entry object.
   },
   module: {
     rules: [
